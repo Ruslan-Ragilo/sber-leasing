f046150765ed5d3794a022a8c286cfad3853cfdc
Ruslan Ragilo <ruslanragilo7@gmail.com>
2023-07-11 18:31:24 +0300

    added animation circle text and fonts

--- index.js
+++
@@ -1,5 +0,0 @@
-const text = document.querySelector(".header-banner__right p");
-
-text.innerHTML = text.innerText.split("").map(
-		(char, i) => `<span style="transform:rotate(${i * 5.8}deg)">${char}</span>`
-	).join("");
