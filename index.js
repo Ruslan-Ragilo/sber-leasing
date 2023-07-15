85b86420c06c4515f3fed4abdf856cfbee85ad73
Ruslan Ragilo <ruslanragilo7@gmail.com>
2023-07-15 10:21:47 +0300

    added map svg

---
+++ index.js
@@ -0,0 +1,76 @@
+const dataMap = [
+  {
+    id: 'VLV'
+  },
+  {
+    id: 'MAG'
+  },
+  {
+    id: 'YAKU'
+  },
+  {
+    id: 'HBR'
+  },
+  {
+    id: 'BLG'
+  },
+  {
+    id: 'CHIT'
+  },
+  {
+    id: 'UDE'
+  },
+  {
+    id: 'IRK'
+  },
+  {
+    id: 'KRR'
+  },
+  {
+    id: 'TOM'
+  },
+  {
+    id: 'КЕМ'
+  },
+  {
+    id: 'YSH'
+  },
+  {
+    id: 'YSH'
+  },
+  {
+    id: 'YSH'
+  },
+  {
+    id: 'YSH'
+  },
+  {
+    id: 'YSH'
+  },
+  {
+    id: 'YSH'
+  },
+  {
+    id: 'YSH'
+  },
+  {
+    id: 'YSH'
+  },
+  {
+    id: 'YSH'
+  },
+  {
+    id: 'YSH'
+  },
+  {
+    id: 'YSH'
+  },
+  {
+    id: 'YSH'
+  },
+  {
+    id: 'YSH'
+  },
+  
+
+]
