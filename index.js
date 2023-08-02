3ccb2f101d10d0fbbfcdaf65120b4e47356cc619
Ruslan Ragilo <ruslanragilo7@gmail.com>
2023-08-02 12:11:26 +0300

    fix tooltip X and Y cordinate

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ import { DataMap } from './constants.js';
 const tooltip = document.getElementById('map-city-tooltip');
 
 const showTooltip = (e, name) => {
+  console.log(e.target.getBoundingClientRect());
   const cityRect = e.target.getBoundingClientRect();
   tooltip.classList.add('visible');
   tooltip.textContent = name;
 
   const tooltipRect = tooltip.getBoundingClientRect();
   const cityPosition = {
-    x: e.target.getBoundingClientRect().left - tooltipRect.width / 2,
-    y: cityRect.y + window.scrollY - tooltipRect.height - 4,
+    x: cityRect.left - tooltipRect.width / 2 + cityRect.width / 2,
+    y: cityRect.y + window.scrollY - tooltipRect.height - 8,
   };
   tooltip.style.left = cityPosition.x + 'px';
   tooltip.style.top = cityPosition.y + 'px';
